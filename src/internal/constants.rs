3db94543e73f9f11ea326e5b19eb42489f83e561
kweonminsung <kevin136583@gmail.com>
2025-09-30 18:18:51 +0900

    feat: add redirect name for algorithms in RndcAlg

--- src/internal/constants.rs
+++ src/internal/constants.rs
@@ -25,11 +25,17 @@ impl RndcAlg {
     pub(crate) fn from_string(alg: &str) -> Option<Self> {
         match alg {
             "md5" => Some(RndcAlg::MD5),
+            "hmac-md5" => Some(RndcAlg::MD5),
             "sha1" => Some(RndcAlg::SHA1),
+            "hmac-sha1" => Some(RndcAlg::SHA1),
             "sha224" => Some(RndcAlg::SHA224),
+            "hmac-sha224" => Some(RndcAlg::SHA224),
             "sha256" => Some(RndcAlg::SHA256),
+            "hmac-sha256" => Some(RndcAlg::SHA256),
             "sha384" => Some(RndcAlg::SHA384),
+            "hmac-sha384" => Some(RndcAlg::SHA384),
             "sha512" => Some(RndcAlg::SHA512),
+            "hmac-sha512" => Some(RndcAlg::SHA512),
             _ => None,
         }
     }
